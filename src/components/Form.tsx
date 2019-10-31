60c835c95f28f3ad55574c22f6935c9ff5d95e04
aratasato <14242642+aratasato@users.noreply.github.com>
2019-10-31 14:57:00 +0900

    add FormProps

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react'
+import { ChangeEvent } from 'react'
 
-const Form = (props: {
-  onHandleChange: (arg0: any) => void
-  q: string | number | string[] | undefined
-  onHandleSubmit:
-    | ((event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void)
-    | undefined
-}) => {
+type FormProps = {
+  onHandleChange: (e: ChangeEvent) => void
+  q: string
+  onHandleSubmit: (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => void
+}
+
+const Form: React.FC<FormProps> = props => {
   const handleChange = (e: any) => {
     props.onHandleChange(e)
   }
