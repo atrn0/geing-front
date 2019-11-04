c302abe6d76e14a6a3ec6191afad6ac3af4db13a
aratasato <14242642+aratasato@users.noreply.github.com>
2019-11-05 00:56:01 +0900

    add images

--- src/components/QuestionsList.tsx
+++ src/components/QuestionsList.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { getQuestions, GetQuestionsRes } from '../client/geing/getQuestions'
 import Question from '../models/question'
+import styles from './QuestionsList.module.scss'
 
 const QuestionsList = () => {
   const [data, setQuestions] = useState({ questions: [] as Question[] })
@@ -20,11 +21,18 @@ const QuestionsList = () => {
 
   return (
     <>
-      <ul>
+      <div className={styles.questions}>
         {data.questions.map((q: Question) => {
-          return <li key={q.id}>{q.body}</li>
+          return (
+            <img
+              alt={q.body}
+              src={`https://res.cloudinary.com/dfrif3y8l/image/upload/w_800/c_fit,q_10,h_300,l_text:omklw31ggvthvlk7b8i3.ttf_40:${encodeURIComponent(
+                q.body
+              )},w_700,y_-15/geing-ogp`}
+            />
+          )
         })}
-      </ul>
+      </div>
     </>
   )
 }
