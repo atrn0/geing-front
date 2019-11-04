00450b5f3618bba71769aa67c4102325ad08f713
aratasato <14242642+aratasato@users.noreply.github.com>
2019-11-05 01:20:26 +0900

    add 回答をみる

--- src/components/QuestionsList.tsx
+++ src/components/QuestionsList.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { getQuestions, GetQuestionsRes } from '../client/geing/getQuestions'
 import Question from '../models/question'
 import styles from './QuestionsList.module.scss'
+import dayjs from 'dayjs'
 
 const QuestionsList = () => {
   const [data, setQuestions] = useState({ questions: [] as Question[] })
@@ -24,12 +25,18 @@ const QuestionsList = () => {
       <div className={styles.questions}>
         {data.questions.map((q: Question) => {
           return (
-            <img
-              alt={q.body}
-              src={`https://res.cloudinary.com/dfrif3y8l/image/upload/w_800/c_fit,q_10,h_300,l_text:omklw31ggvthvlk7b8i3.ttf_40:${encodeURIComponent(
-                q.body
-              )},w_700,y_-15/geing-ogp`}
-            />
+            <div key={q.id}>
+              <span>{dayjs(q.created_at).format('MM/DD')}</span>
+              <img
+                alt={q.body}
+                src={`https://res.cloudinary.com/dfrif3y8l/image/upload/w_800/c_fit,q_10,h_300,l_text:omklw31ggvthvlk7b8i3.ttf_40:${encodeURIComponent(
+                  q.body
+                )},w_700,y_-15/geing-ogp`}
+              />
+              <div>
+                <a href="#">回答をみる</a>
+              </div>
+            </div>
           )
         })}
       </div>
