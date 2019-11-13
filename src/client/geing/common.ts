d78bec9d52467e4a67985b26ac57e75ba562ab9b
aratasato <14242642+aratasato@users.noreply.github.com>
2019-11-13 23:39:57 +0900

    add .env

--- src/client/geing/common.ts
+++ src/client/geing/common.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const geingInstance = axios.create({
-  baseURL: process.env.GEING_BASE_URL || 'http://localhost:9090/'
+  baseURL: process.env.REACT_APP_BASE_URL || 'http://localhost:8080/'
 })
