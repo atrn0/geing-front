752fd85c85059f4eb3f7c0187ad5e7788559f39d
aratasato <14242642+aratasato@users.noreply.github.com>
2019-11-15 20:51:08 +0900

    質問に/が含まれていたらバグる問題を修正

--- src/components/QuestionsList.tsx
+++ src/components/QuestionsList.tsx
@@ -40,7 +40,7 @@ const QuestionsList = () => {
             <img
               alt={q.body}
               src={`https://res.cloudinary.com/dfrif3y8l/image/upload/w_800/c_fit,q_10,h_300,l_text:omklw31ggvthvlk7b8i3.ttf_40:${encodeURIComponent(
-                q.body
+                encodeURIComponent(q.body)
               )},w_700,y_-15/geing-ogp`}
             />
             <div>
